68f75fa7f52e140681b05b66174b0c0629e538a0
zain00abd <zainabflash1998@gmail.com>
2023-10-15 22:19:51 +0300

    yola

--- app/components/Allcard.tsx
+++ app/components/Allcard.tsx
@@ -3,7 +3,7 @@ import Card from './card'
 function Allcard(){
 return(
     <div className='row'>
-        <div className='col-12'>
+        <div className='col-12 mb-4'>
             <Card/>
             <Card/>
             <Card/>
