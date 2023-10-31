7e54eda9f0e3962c670e2c2f44b95ee843742122
zain00abd <zainabflash1998@gmail.com>
2023-10-31 22:33:24 +0200

    Update page.tsx

--- app/Haloo/page.tsx
+++ app/Haloo/page.tsx
@@ -8,15 +8,6 @@ function Halo() {
                 <div className="container-fluid">
                 </div>
             </nav>
-            <div className="container ">
-                <div className="row-g" style={{ width: "75%", marginLeft: "13%", marginTop: "15px" }}>
-                    <div className="card col-12">
-                        <div className="card-body">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </>
 
 
