0f651e39dc2e209b7d4ee791f7fe433c71722887
zain00abd <zainabflash1998@gmail.com>
2023-11-01 21:12:27 +0200

    Update Cardv.tsx

--- app/components/Cardv.tsx
+++ app/components/Cardv.tsx
@@ -14,7 +14,7 @@ function Cardv() {
           <Card>
             <video controls src='https://cdn.busuu.com/media-resources/video/mp4/fb464cc9-8a5b-4548-9bac-a42fae718f42_small.mp4'/>
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>zain title</Card.Title>
               <Card.Text>
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
