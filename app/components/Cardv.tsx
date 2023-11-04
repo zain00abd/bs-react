7e061ede1a47e5c8b830bb0a23e4e55d974cac8f
zain00abd <zainabflash1998@gmail.com>
2023-11-04 23:57:13 +0200

    Update Cardv.tsx

--- app/components/Cardv.tsx
+++ app/components/Cardv.tsx
@@ -4,9 +4,27 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import "./card.css"
+import { useRef, useState } from 'react';
 
 
 function Cardv() {
+
+  const btnEr = useRef<HTMLButtonElement>(null)
+
+  const [pool, setinner] = useState('')
+
+  const isinner = () => {
+    setinner("dhfe")
+
+    if(btnEr.current){
+
+      btnEr.current.style.backgroundColor = '#ff0000'
+      btnEr.current.style.color = '#ffffff'
+    }
+  }
+  console.log(pool)
+
+
   return (
     <>
       <Row className="g-4 mt-30" id="cardz">
@@ -27,8 +45,8 @@ function Cardv() {
 
       <div className='container mt-4'>
         <div className='row justify-content-evenly' >
-          <button type="button" className="btn btn-outline-secondary btn-lg col-4">Large button</button>
-          <button type="button" className="btn btn-outline-secondary text-dark btn-lg col-4">Large button</button>
+          <button type="button" ref={btnEr} onClick={isinner} className="btn btn-outline-secondary btn-lg col-4">Large button</button>
+          <button type="button" onClick={isinner} className="btn btn-outline-secondary text-dark btn-lg col-4">Large button</button>
         </div>
       </div>
     </>
